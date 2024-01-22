06500a4277831fa3c3dd01b3249cf0b45e9cefdb
how2fps <hearthstonesigh@gmail.com>
2024-01-22 14:54:41 +0800

    further updated portfolio, left with projects page

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-       console.log("hi");
-       var fixedBox = document.querySelector(".moving-rectangle");
+       const logoText = document.querySelector(".logo-text");
+       logoText.innerHTML = logoText.innerText
+              .split("")
+              .map((char, i) => `<span style="transform:rotate(${i * 16}deg)">${char}</span>`)
+              .join("");
+
+       const scrollbar = document.querySelector(".scrollbar");
 
        window.addEventListener("scroll", function () {
-              // Get the scroll position
-              var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-
-              // Show/hide the fixed box based on the scroll position
-              // Move the fixed box horizontally based on the scroll position
-              fixedBox.style.right = 435 - scrollPosition / 8 + "px";
+              // Calculate the percentage scrolled
+              const scrollPercentage = (window.scrollY / window.innerHeight) * 100;
+              // Update the scroll-section's position
+              scrollbar.style.transform = "translateY(" + scrollPercentage + "%)";
        });
+
+       const mainContainer = document.querySelector(".main");
+       const scrollText = document.querySelector(".scroll-text");
+       let isScrolling = false;
+       // Listen for the mouse wheel event
+       mainContainer.addEventListener(
+              "wheel",
+              function (event) {
+                     event.preventDefault();
+                     // Check if scrolling is not already in progress
+                     if (!isScrolling) {
+                            isScrolling = true;
+
+                            // Calculate the distance to scroll instantly (set to 100vh)
+                            const scrollAmount = window.innerHeight;
+
+                            // Determine the direction of the scroll
+                            const delta = Math.sign(event.deltaY);
+
+                            if (delta === 1) {
+                                   scrollText.style.opacity = "0";
+                                   scrollText.style.animation = "slideToRight 0.8s";
+                            }
+
+                            // Scroll instantly
+                            window.scrollBy(0, delta * scrollAmount);
+
+                            // Set a timeout to reset the scrolling flag after a short delay
+                            setTimeout(function () {
+                                   isScrolling = false;
+                            }, 100);
+                     }
+              },
+              { passive: false }
+       );
 });
